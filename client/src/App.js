c51b81395c336ecce8186c161e3dd72a3309ee42
ammarmian94 <96662437+ammarmian94@users.noreply.github.com>
2024-01-27 01:14:04 +0500

    Working on Pages & Components

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,29 @@
-import React from 'react';
-import Counter from './features/counter/Counter';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Home from "./pages/Home";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import ProductDetailsPage from "../src/pages/ProductDetailsPage";
+import ProductListPage from "./pages/ProductListPage";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home></Home>,
+  },
+  {
+    path: "/products",
+    element: <ProductListPage></ProductListPage>
+  },
+  {
+    path: "/product-detail",
+    element: <ProductDetailsPage></ProductDetailsPage>,
+  },
+]);
 
 function App() {
   return (
     <div className="App">
-      <Counter></Counter>
+      <RouterProvider router={router} />
     </div>
   );
 }
